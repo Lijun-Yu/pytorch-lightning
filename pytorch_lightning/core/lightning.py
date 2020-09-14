e387a0b56dcc33a04bf9ebbc64abb57348de0974
Lijun Yu <lijun@lj-y.com>
2020-09-14 14:37:40 -0400

    slurm root node

--- pytorch_lightning/core/lightning.py
+++ pytorch_lightning/core/lightning.py
@@ -864,7 +864,7 @@ class LightningModule(ABC, DeviceDtypeModuleMixin, GradInformation, ModelIO, Mod
 
         # figure out the root node addr
         try:
-            root_node = os.environ['SLURM_NODELIST'].split(' ')[0]
+            root_node = os.environ['SLURM_SRUN_COMM_HOST']
         except Exception:
             root_node = '127.0.0.1'
 
